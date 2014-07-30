38896dcc548032e7bfab70927a0d6a5cd7ea9e97
Blake Mason & Anna Valkov <student@epicodus.com>
2014-07-30 14:40:25 -0700

    adds additional features and feedback in UI

--- gallows_spelling.rb
+++ gallows_spelling.rb
@@ -2,15 +2,21 @@ require "./lib/SecretWord"
 # require "./lib/body"
 
 def main_menu
-  puts "Enter a Secret Word to play Gallows Spelling"
+  puts "\e[H\e[2J"
+  puts "Enter a Secret Word to play Gallows Spelling\nPress '#' to exit the program at any time."
   user_secret = gets.chomp.upcase
+  if user_guess == "#" #escape route
+    exit
+  elsif user_secret.match(/[a-zA-Z]/) == nil
+    main_menu
+  end
   secret_word = SecretWord.new(user_secret)
   puts "\e[H\e[2J"
   spaces = ["_"] * user_secret.length
   puts spaces.join(" ")
 
   loop do
-    puts "Guess a letter"
+    puts "\nGuess a letter"
     user_guess = gets.chomp.upcase
     secret_word.guess(user_guess)
     if user_guess == "#" #escape route
@@ -18,35 +24,31 @@ def main_menu
     end
     if secret_word.wrong_letters.include?(user_guess) || secret_word.right_letters.include?(user_guess)
       puts "You already guessed that letter."
-    elsif user_guess.length > 1 || user_guess.match(/\d/)
+    elsif user_guess.length > 1 || user_guess.match(/[a-zA-Z]/) == nil
       puts "Only one letter please."
     else
       secret_word.letter_checker
-      puts "Wrong guesses: #{secret_word.wrong_letters.join(", ")}"
+      puts "\e[H\e[2J"
       secret_word.letter_position.each do |index|
         spaces[index] = user_guess
       end
       puts spaces.join(" ")
+      if spaces.include?("_") && secret_word.wrong_letters.length > 0
+        puts "\nWrong guesses: #{secret_word.wrong_letters.join(", ")}"
+      elsif spaces.include?("_") == false
+        puts "You guessed the Secret Word!\nPress any key to play again."
+        if gets
+          main_menu
+        end
+      end
       if secret_word.wrong_letters.length >= 7
-        puts "Game Over"
-        main_menu
+        puts "\nGame Over.  Too many guesses.  \nPress any key to play again."
+        if gets
+          main_menu
+        end
       end
     end
   end
 end
-
-# def guessing
-#   loop do
-#     puts "Guess a letter"
-#     user_guess = gets.chomp.upcase
-#     secret_word.guess(user_guess)
-#     secret_word.letter_checker
-#     puts "#{secret_word.wrong_letters}"
-#     if secret_word.wrong_letters >= 7
-#       puts "Game Over"
-#       main_menu
-#     end
-#   end
-# end
 
 main_menu
